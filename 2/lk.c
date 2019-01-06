3d2aff159241495880063a55480cbe5fdfe0ac99
Mohit Kumar Toshniwal <mohitkrtoshniwal@gmail.com>
2019-01-06 22:06:10 +0530

    Update lk.c

--- 2/lk.c
+++ 2/lk.c
@@ -1,46 +1,49 @@
 #include<stdio.h>
-struct node
-{
-unsigned dist[20];
-unsigned from[20];
-}rt[10];
+#include<stdlib.h>
+#define MIN(x,y) (x>y)?y:x
+
 int main()
 {
-int costmat[20][20];
-int nodes,i,j,k,count=0;
-printf("\nEnter the number of nodes : ");
-scanf("%d",&nodes);//Enter the nodes
-printf("\nEnter the cost matrix :\n");
-for(i=0;i<nodes;i++)
-{
-    for(j=0;j<nodes;j++)
-    {
-    scanf("%d",&costmat[i][j]);
-    costmat[i][i]=0;
-    rt[i].dist[j]=costmat[i][j];
-    rt[i].from[j]=j;
-    }
-}
-do
-{
-    count=0;
-    for(i=0;i<nodes;i++)
-    for(j=0;j<nodes;j++)
-    for(k=0;k<nodes;k++)
-        if(rt[i].dist[j]>costmat[i][k]+rt[k].dist[j])
-        {
-        rt[i].dist[j]=rt[i].dist[k]+rt[k].dist[j];
-        rt[i].from[j]=k;
-        count++;
-        }
-}while(count!=0);
-printf("Enter dest\n");
-scanf("%d",&i);
-printf("\n\n For router %d\n",i);
-for(j=0;j<nodes;j++)
-{
-printf("\t\nnode %d via %d Distance %d",j+1,rt[i-1].from[j]+1,rt[i-1].dist[j]);
-}
+int orate,drop=0,cap,x,count=0,inp[10]={0},i=0,nsec,ch;
+printf(" \n enter bucket size : ");
+scanf("%d",&cap);
+printf("\n enter output rate :");
+scanf("%d",&orate);
+  
+do{
+printf("\n enter number of packets coming at second %d : ",i+1);
+scanf("%d",&inp[i]);
+i++;
+printf("\n enter 1 to contiue or 0 to quit..........");
+scanf("%d",&ch);
+}while(ch);
 
-printf("\n\n");
+nsec=i;
+printf("\n second \t recieved \t sent \t dropped \t remained \n");
+for(i=0;count || i<nsec;i++)
+{
+ printf("%d",i+1);
+ printf(" \t %d\t ",inp[i]);
+ printf(" \t %d\t ",MIN((inp[i]+count),orate));
+ if((x=inp[i]+count-orate)>0)
+ {
+  if(x>cap)
+  {
+   count=cap;
+   drop=x-cap;
+  }
+  else
+  {
+   count=x;
+   drop=0;
+  }
+ }
+ else
+ {
+  drop=0;
+  count=0;
+  }
+ printf(" \t %d \t %d \n",drop,count);
+ }   // end of for loop
+ return 0;
 }
